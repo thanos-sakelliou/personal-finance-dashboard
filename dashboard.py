3c0ee2b1e14c60c8bcd16cede09ba8bd90d18162
thanos-sakelliou <thanos.sakelliou@gmail.com>
2024-10-19 15:17:22 +0300

    created piraeus dataloader class

--- dashboard.py
+++ dashboard.py
@@ -4,21 +4,11 @@ from dash import dcc, html
 from dash.dependencies import Input, Output
 import plotly.graph_objs as go
 import plotly.express as px
+import numpy as np
 
-# Load and preprocess data
-def load_data(filepath):
-    df = pd.read_excel(filepath, skiprows=5)
-    df = df.dropna()
-    df.columns = ['Category', 'Transaction Description', 'Transaction Date', 
-                  'Comments', 'Amount', 'Currency', 'Progressive Balance', 'Currency_Duplicate']
-    df = df.drop(columns=['Currency_Duplicate'])
-    df['Transaction Date'] = pd.to_datetime(df['Transaction Date'], format='%d/%m/%Y')
-    df['Amount'] = pd.to_numeric(df['Amount'], errors='coerce')
-    df['Progressive Balance'] = pd.to_numeric(df['Progressive Balance'], errors='coerce')
-    return df
 
-# Define an improved color map for categories and subcategories
 def get_color(category):
+# Define an improved color map for categories and subcategories
     color_map = {
         'Savings': '#1E90FF',        # Blue
         'Income': '#32CD32',         # Lime Green
@@ -35,78 +25,6 @@ def get_color(category):
         'Travels': '#FF6347'         # Tomato
     }
     return color_map.get(category, '#d3d3d3')  # Default to light grey if not in map
-
-def map_supercategories(df):
-    # Define the category to supercategory mapping
-    category_mapping = {
-        'Ανακατανομή': 'Savings',
-        'Έσοδα / Έσοδα': 'Income',
-        'Μετακίνηση / Αυτοκίνητο': 'Transportation',
-        'Μετακίνηση / Δρομολόγια': 'Transportation',
-        'Μετακίνηση / Εισιτήρια': 'Transportation',
-        'Μετακίνηση / Parking': 'Transportation',
-        'Μετακίνηση / Καύσιμα': 'Transportation',
-        'Μετρητά / Αναλήψεις': 'Uncategorised',
-        'Μετρητά / Εμβάσματα': 'Uncategorised',
-        'Μετρητά / Μεταφορές': 'Uncategorised',
-        'Σπίτι / Άλλο': 'Uncategorised',
-        'Σπίτι / Ενοίκιο': 'Housing',
-        'Σπίτι / Ενέργεια': 'Electricity',
-        'Σπίτι / Εξοπλισμός': 'Shopping',
-        'Σπίτι / Supermarket': 'Food & Consumables',
-        'Σπίτι / Επικοινωνίες': 'Subscriptions',
-        'Υγεία / Ιατρικά': 'Health',
-        'Υγεία / Υπηρεσίες': 'Health',
-        'Υγεία / Φαρμακεία': 'Health',
-        'Υποχρεώσεις / Προμήθειες': 'Uncategorised',
-        'Υποχρεώσεις / Φόροι': 'Taxes & obligations',
-        'Υποχρεώσεις / Υπηρεσίες': 'Taxes & obligations',
-        'Ψυχαγωγία / Άθληση': 'Subscriptions',
-        'Ψυχαγωγία / Διασκέδαση': 'Entertainment',
-        'Ψυχαγωγία / Εστιατόρια': 'Food & Consumables',
-        'Ψυχαγωγία / Καθημερινά': 'Food & Consumables',
-        'Ψυχαγωγία / Συνδρομές': 'Subscriptions',
-        'Ψυχαγωγία / Ταξίδια': 'Travels',
-        'Ψυχαγωγία / Χόμπυ': 'Shopping',
-        'Ψώνια / Αξεσουάρ': 'Shopping',
-        'Ψώνια / Άλλο': 'Shopping',
-        'Ψώνια / Ηλεκτρονικά': 'Shopping',
-        'Ψώνια / Ρουχισμός': 'Shopping',
-        'Χωρίς Κατηγορία': 'Uncategorised'
-    }
-    
-    # Define keywords for specific supercategories
-    keyword_mapping = {
-        'Subscriptions': ['spotify', 'audible', 'netflix'],
-        'Savings': ['revolut'],
-        # 'Health': ['pharmacy', 'doctor', 'medical'],
-        # Add more supercategories and keywords as needed
-    }
-
-    # Function to check if any keyword matches in the description
-    def check_keywords(description):
-        description = description.lower()  # Convert to lowercase for case-insensitive matching
-        for supercategory, keywords in keyword_mapping.items():
-            if any(keyword in description for keyword in keywords):
-                return supercategory
-        return None
-
-
-    # Define a function to apply the mapping and rules based on transaction description and amount
-    def apply_rules(row):
-        # Check for keywords in the description
-        supercategory_from_keywords = check_keywords(row['Comments'])
-        if supercategory_from_keywords:
-            return supercategory_from_keywords
-        # elif abs(row['Amount']) > 1000:
-        #     return 'Large Transfers'
-
-        return category_mapping.get(row['Category'], 'Other')
-
-    # Apply the rules to each row and create a new column for the supercategory
-    df['Supercategory'] = df.apply(apply_rules, axis=1)
-
-    return df
 
 
 def create_balance_chart(df, threshold=100):
@@ -155,7 +73,7 @@ def create_balance_chart(df, threshold=100):
     
     # Update layout
     fig.update_layout(
-        title='Account Balance Over Time with High-Value Transactions',
+        title='Blow Account Balance',
         xaxis_title='Date',
         yaxis_title='Account Balance (€)',
         showlegend=True
@@ -167,14 +85,15 @@ def create_monthly_balance_change_bar_chart(df):
     # Extract Year-Month for grouping by month
     df['Year-Month'] = df['Transaction Date'].dt.to_period('M')
 
-    # Exclude the "Savings" category
-    df_no_savings = df[df['Supercategory'] != 'Savings']
-
     # Calculate the account balance at the end of each month
-    monthly_balance = df_no_savings.groupby('Year-Month')['Progressive Balance'].last().reset_index()
+    # Identify the last transaction for each month without altering the order of the DataFrame
+    monthly_indices = df.groupby('Year-Month').head(1).index
+    
+    # Extract these rows while preserving the order of appearance in the original DataFrame
+    monthly_balance = df.loc[monthly_indices, ['Year-Month', 'Progressive Balance']].reset_index(drop=True)
 
     # Calculate monthly changes in balance
-    monthly_balance['Monthly Change'] = monthly_balance['Progressive Balance'].diff().fillna(0)
+    monthly_balance['Monthly Change'] = monthly_balance['Progressive Balance'][::-1].diff()[::-1].fillna(0)
 
     # Round the values for clarity
     monthly_balance['Progressive Balance'] = monthly_balance['Progressive Balance'].round(0)
@@ -208,7 +127,7 @@ def create_monthly_balance_change_bar_chart(df):
 
     # Update layout
     fig.update_layout(
-        title='Monthly Balance Changes and Account Balance at Month-End',
+        title='Monthly Gains and losses',
         xaxis_title='Month',
         yaxis_title='Amount (€)',
         showlegend=True
@@ -298,21 +217,201 @@ app.layout = html.Div([
     [Input('balance-chart', 'id')]
 )
 def update_graphs(_):
-    df = load_data('transactions.xlsx')
-    df = map_supercategories(df)
-    balance_chart = create_balance_chart(df, threshold=50)
-    monthly_balance_change_bar_chart = create_monthly_balance_change_bar_chart(df)
-    savings_goal_gauge = create_savings_goal_gauge(df)
-    monthly_expenses_bar_chart = create_monthly_expenses_bar_chart(df)
-    all_time_expenses_bar_chart = create_all_time_expenses_bar_chart(df)
+    loader = Piraeus_data_loader('PiraeusStatement.xlsx')
+    loader.digest_bank_statement('Κινήσεις Λογαριασμών_20241018.xlsx')
+    loader.digest_bank_statement('Κινήσεις Λογαριασμών_20241016.xlsx')
+    balance_df = loader.get_balance_df()
+    expenses_df = loader.get_expenses_df()
+    balance_chart = create_balance_chart(balance_df, threshold=50)
+    monthly_balance_change_bar_chart = create_monthly_balance_change_bar_chart(expenses_df)
+    
+    savings_goal_gauge = create_savings_goal_gauge(balance_df)
+    monthly_expenses_bar_chart = create_monthly_expenses_bar_chart(balance_df)
+    all_time_expenses_bar_chart = create_all_time_expenses_bar_chart(balance_df)
     return balance_chart, monthly_balance_change_bar_chart, savings_goal_gauge, monthly_expenses_bar_chart, all_time_expenses_bar_chart
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
-    # df = load_data('transactions.xlsx')
-    # print(df)
-    # df = map_supercategories(df)
 
+class Piraeus_data_loader:
+    def __init__(self, filepath):
+        self.filepath = filepath
+        # pd.options.display.datetime_format = '%d/%m/%Y' 
+        self.df = self.load_from_excel()
+
+
+    def load_from_excel(self):
+        # Load the clean data from the Excel file
+        df = pd.read_excel(self.filepath, header=0)
+        self.column_names = df.columns.tolist()
+        df['Transaction Date'] = pd.to_datetime(df['Transaction Date'], format='%d/%m/%Y') #.dt.strftime('%d/%m/%Y')
+        df['Amount'] = pd.to_numeric(df['Amount'], errors='coerce')
+        df['Progressive Balance'] = pd.to_numeric(df['Progressive Balance'], errors='coerce')
+        return df
+    
+
+    def digest_bank_statement(self, filepath):
+        # Load additional transactions from another Excel file
+        new_df = pd.read_excel(filepath, skiprows=5, skipfooter=3)
+        
+        # Check if 'Progressive Balance' is present in the new data
+        if len(new_df.columns) == 6:
+            # in case progressive balance is missing
+            new_df.columns = self.column_names[:-2]
+            new_df['Progressive Balance'] = None  # Placeholder for now
+        else:
+            if len(new_df.columns) == 8:
+                new_df = new_df.drop(new_df.columns[-1], axis=1)
+            # Set column names for data with 'Progressive Balance'
+            new_df.columns = self.column_names[:-1]
+            new_df['Progressive Balance'] = pd.to_numeric(new_df['Progressive Balance'], errors='coerce')
+        
+        # Convert types
+        new_df['Transaction Date'] = pd.to_datetime(new_df['Transaction Date'], format='%d/%m/%Y') #.dt.strftime('%d/%m/%Y')
+        new_df['Amount'] = pd.to_numeric(new_df['Amount'], errors='coerce')
+        
+        #add supercategory column
+        new_df = self.map_supercategories(new_df)
+        
+        # Determine where the new data should be appended
+        if new_df['Transaction Date'].max() <= self.df['Transaction Date'].max():
+            # If all dates in df2 are before the first date in df1, append df2 at the top
+            combined_df = pd.concat([self.df, new_df]).drop_duplicates(subset=['Transaction Date', 'Amount', 'Comments']).reset_index(drop=True)
+        elif new_df['Transaction Date'].min() >= self.df['Transaction Date'].min():
+            # If all dates in df2 are after the last date in df1, append df2 at the bottom
+            combined_df = pd.concat([new_df,self.df]).drop_duplicates(subset=['Transaction Date', 'Amount', 'Comments']).reset_index(drop=True)
+        else:
+            # If there's overlap in the date ranges, raise an error or handle accordingly
+            raise ValueError("The dates in df2 overlap with the dates in df1")
+
+        # Calculate the progressive balance if missing
+        if combined_df['Progressive Balance'].isna().any():
+            combined_df = self.calculate_progressive_balance(combined_df)
+
+        self.df = combined_df
+    
+    
+    def save_to_excel(self):
+        export_df = self.df
+        export_df['Transaction Date'] = export_df['Transaction Date'].dt.strftime('%d/%m/%Y')
+        export_df.to_excel(self.filepath, index=False)
+    
+
+    def calculate_progressive_balance(self, df):
+        # Check if 'Progressive Balance' column exists, if not, create it with NaNs
+        if 'Progressive Balance' not in df.columns:
+            df['Progressive Balance'] = np.nan
+        
+        # Fill the missing 'Progressive Balance' values based on available data
+        for i in range(len(df)):
+            # If 'Progressive Balance' is NaN
+            if pd.isna(df.loc[i, 'Progressive Balance']):
+                # Find the previous and next available balances
+                prev_balance = df.loc[i - 1, 'Progressive Balance'] if i > 0 else np.nan
+                # next_balance = df.loc[i + 1, 'Progressive Balance'] if i < len(df) - 1 else np.nan
+                prev_amount = df.loc[i-1, 'Amount']
+        
+                df.loc[i, 'Progressive Balance'] = prev_balance - prev_amount
+        return df
+    
+    def update_progressive_balance(self, df):
+        # Start with the last transaction and work backwards
+        for i in range(len(df) - 2, 0, -1):
+            # Get the next row's progressive balance and the current transaction amount using iloc
+            prev_balance = df.iloc[i+1]['Progressive Balance']
+            amount = df.iloc[i]['Amount']
+            
+            # Calculate the previous row's progressive balance as the next balance minus the amount
+            df.iloc[i, df.columns.get_loc('Progressive Balance')] = prev_balance + amount
+        
+
+        return df
+            
+    def get_balance_df(self):
+        return self.df   
+    
+    def get_expenses_df(self):
+        # Exclude the "Savings" category
+        expenses_df = self.df[self.df['Supercategory'] != 'Savings']
+        expenses_df = expenses_df.reset_index(drop=True)
+        expenses_df = self.update_progressive_balance(expenses_df)
+        return expenses_df
+   
+
+    def map_supercategories(self, df):
+        # Define the category to supercategory mapping
+        category_mapping = {
+            'Ανακατανομή': 'Savings',
+            'Έσοδα / Έσοδα': 'Income',
+            'Μετακίνηση / Αυτοκίνητο': 'Transportation',
+            'Μετακίνηση / Δρομολόγια': 'Transportation',
+            'Μετακίνηση / Εισιτήρια': 'Transportation',
+            'Μετακίνηση / Parking': 'Transportation',
+            'Μετακίνηση / Καύσιμα': 'Transportation',
+            'Μετρητά / Αναλήψεις': 'Uncategorised',
+            'Μετρητά / Εμβάσματα': 'Uncategorised',
+            'Μετρητά / Μεταφορές': 'Uncategorised',
+            'Σπίτι / Άλλο': 'Uncategorised',
+            'Σπίτι / Ενοίκιο': 'Housing',
+            'Σπίτι / Ενέργεια': 'Electricity',
+            'Σπίτι / Εξοπλισμός': 'Shopping',
+            'Σπίτι / Supermarket': 'Food & Consumables',
+            'Σπίτι / Επικοινωνίες': 'Subscriptions',
+            'Υγεία / Ιατρικά': 'Health',
+            'Υγεία / Υπηρεσίες': 'Health',
+            'Υγεία / Φαρμακεία': 'Health',
+            'Υποχρεώσεις / Προμήθειες': 'Uncategorised',
+            'Υποχρεώσεις / Φόροι': 'Taxes & obligations',
+            'Υποχρεώσεις / Υπηρεσίες': 'Taxes & obligations',
+            'Ψυχαγωγία / Άθληση': 'Subscriptions',
+            'Ψυχαγωγία / Διασκέδαση': 'Entertainment',
+            'Ψυχαγωγία / Εστιατόρια': 'Food & Consumables',
+            'Ψυχαγωγία / Καθημερινά': 'Food & Consumables',
+            'Ψυχαγωγία / Συνδρομές': 'Subscriptions',
+            'Ψυχαγωγία / Ταξίδια': 'Travels',
+            'Ψυχαγωγία / Χόμπυ': 'Shopping',
+            'Ψώνια / Αξεσουάρ': 'Shopping',
+            'Ψώνια / Άλλο': 'Shopping',
+            'Ψώνια / Ηλεκτρονικά': 'Shopping',
+            'Ψώνια / Ρουχισμός': 'Shopping',
+            'Χωρίς Κατηγορία': 'Uncategorised'
+        }
+        
+        # Define keywords for specific supercategories
+        keyword_mapping = {
+            'Subscriptions': ['spotify', 'audible', 'netflix'],
+            'Savings': ['revolut'],
+            # 'Health': ['pharmacy', 'doctor', 'medical'],
+            # Add more supercategories and keywords as needed
+        }
+    
+        # Function to check if any keyword matches in the description
+        def check_keywords(description):
+            description = description.lower()  # Convert to lowercase for case-insensitive matching
+            for supercategory, keywords in keyword_mapping.items():
+                if any(keyword in description for keyword in keywords):
+                    return supercategory
+            return None
+    
+        # Define a function to apply the mapping and rules based on transaction description and amount
+        def apply_rules(row):
+            # Check for keywords in the description
+            supercategory_from_keywords = check_keywords(row['Comments'])
+            if supercategory_from_keywords:
+                return supercategory_from_keywords
+            # elif abs(row['Amount']) > 1000:
+            #     return 'Large Transfers'
+    
+            return category_mapping.get(row['Category'], 'Other')
+    
+        # Apply the rules to each row and create a new column for the supercategory
+        df['Supercategory'] = df.apply(apply_rules, axis=1)
+    
+        return df
+
+
+
+if __name__ == '__main__':
+    
+    app.run_server(debug=True)
 
 #TODO bigger graph. 
 #TODO remove or change or add the trend heatmap
